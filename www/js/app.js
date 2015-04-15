839674879dd3547d85c80a1dedb23fa8f9d9877d
Vincent <tqm397@gmail.com>
2015-04-14 20:28:56 -0400

    Renamed a bunch of stuff. Also added a test state to see if I can reach google. It works so I think this'll be put into place when we can reach the updated website.

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('app', ['ionic', 'app.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -43,7 +43,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     url: "/browse",
     views: {
       'menuContent': {
-        templateUrl: "templates/browse.html"
+        templateUrl: "http://google.com/"
       }
     }
   })
